e33aa3a76fff61bdfeb0db49bae80277ea62600b
harshrajhrj <rajharsh.footballer@gmail.com>
2025-07-23 00:26:57 +0530

    added code files

--- dijsktra-algo/03 shortest_path_dag.cpp
+++ dijsktra-algo/03 shortest_path_dag.cpp
@@ -1,6 +1,14 @@
 /**
  * @file 02 topological_sort.cpp
  * Shortest Path in Directed Acyclic Graph
+ * Following is complete algorithm for finding shortest distances. 
+
+    1. Initialize dist[] = {INF, INF, ....} and dist[s] = 0 where s is the source vertex. 
+    2. Create a topological order of all vertices. 
+    3. Do following for every vertex u in topological order. 
+        ...........Do following for every adjacent vertex v of u 
+        ..................if (dist[v] > dist[u] + weight(u, v)) 
+        ...........................dist[v] = dist[u] + weight(u, v) 
  */
 #include <bits/stdc++.h>
 using namespace std;
