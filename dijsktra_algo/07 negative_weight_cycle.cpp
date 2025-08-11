91d9871c7ee7f8d56629871053f497e7bae00996
harshrajhrj <rajharsh.footballer@gmail.com>
2025-08-11 16:43:38 +0530

    added problem 07

--- dijsktra_algo/07 negative_weight_cycle.cpp
+++ dijsktra_algo/07 negative_weight_cycle.cpp
@@ -13,7 +13,7 @@ struct Graph
     struct Graph *createGraph;
 };
 
-Graph * ::createGraph(int V, int E)
+Graph* createGraph(int V, int E)
 {
     struct Graph *graph = new Graph;
     graph->V = V;
@@ -22,19 +22,89 @@ Graph * ::createGraph(int V, int E)
     return graph;
 }
 
-void findNegCycle(Graph* graph, vector<int> vis, int src)
+void findNegCycle(Graph* graph, vector<int> &vis, int src)
 {
     int V = graph->V;
     int E = graph->E;
 
-    vector<int> dist(src, INT_MAX);
-    vector<int> parent(src)
+    vector<int> dist(V, INT_MAX);
+    vector<int> parent(V, -1);
+
+    dist[src]=0;
+    vis[src]=0;
+
+    bool flag=true; // it means relaxation is still happening for the |V| - 1 iterations
+
+    // relax edges |V| - 1 times
+
+    for(int i=0; i<V; i++)
+    {
+        if(flag==false)
+            break;
+        flag=false;
+        for(int j=0; j<E; j++)
+        {
+            // pick the edge
+            int u=graph->edge[j].src;
+            int v=graph->edge[j].dest;
+            int weight=graph->edge[j].weight;
+
+            if(dist[u] != INT_MAX && dist[u] + weight < dist[v])
+            {
+                vis[v] = 1;
+                dist[v] = dist[u] + weight;
+                parent[v] = u;
+                flag = true;
+            }
+        }
+    }
+
+
+    // Now check for the Vth iteration
+    int C=-1;
+    for(int j=0; j<E; j++)
+    {
+        int u=graph->edge[j].src;
+        int v=graph->edge[j].dest;
+        int weight=graph->edge[j].weight;
+
+        if(dist[u] != INT_MAX && dist[u] + weight < dist[v])
+        {
+            C=v; // this vertex is one of the negative weight cycle
+            break;
+        }
+    }
+
+    if(C != -1)
+    {
+        for (int i = 0; i < V; i++)
+            C = parent[C]; // to make sure that we are reaching a cycle | covering all nodes if every node is involved in a cycle
+
+        // To store the cycle vertex
+        vector<int> cycle;
+        for (int v = C;; v = parent[v]) {
+
+            cycle.push_back(v);
+            if (v == C
+                && cycle.size() > 1)
+                break;
+        }
+
+        // Reverse cycle[]
+        reverse(cycle.begin(), cycle.end());
+
+        // Printing the negative cycle
+        for (int v : cycle)
+            cout << v << ' ';
+        cout << endl;
+       return;
+    }
 }
 
 int main()
 {
-    int V = 5;
-    int E = 5;
+    int V = 8;
+    int E = 8;
 
     struct Graph *graph = createGraph(V, E);
     vector<int> vis(V, 0);
@@ -71,11 +141,13 @@ int main()
     graph->edge[7].dest = 5;
     graph->edge[7].weight = -1;
 
-    // Function Call
+    // Run Bellman–Ford algorithm from each vertex as the source
+    // and check for any negative-weight cycle
     for (int src = 0; src < V; src++)
         if (vis[src] == 0)
             findNegCycle(graph, vis, src);
-    cout << "-1\n";
+    
+    // cout << "-1\n";
 
     return 0;
 }
