2e7ed9dfb50779321deefd80f2e5cf0e7fd4216b
harshrajhrj <rajharsh.footballer@gmail.com>
2025-08-28 15:27:05 +0530

    added tree basics

---
+++ tree_problems/tree.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(x, to) for (x = 0; x < (to); x++)
+#define FORI(x, to) for (x; x < (to); x++)
+#define FORR(x, arr) for (auto &x : arr)
+#define ALL(a) (a.begin(), a.end())
+#define mem(a, n) memset(a, n, sizeof(a))
+
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+void addEdge(int a, int b, vii &adj)
+{
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+}
+
+void printParents(int node, int parent, vii &adj)
+{
+    if (parent == 0)
+        cout << node << " Root" << endl;
+    else
+        cout << node << " -> " << parent << endl;
+
+    // Using DFS to print parent of every node
+    for (auto &e : adj[node])
+    {
+        if (e != parent)
+            printParents(e, node, adj);
+    }
+}
+
+void printLeafNodes(int node, vii &adj)
+{
+    int i = 1;
+    FORI(i, adj.size())
+    {
+        if (adj[i].size() == 1 && i != node)
+            cout << i << " ";
+    }
+    cout << endl;
+}
+
+void printChildren(int node, vii &adj)
+{
+    queue<int> q;
+    vi visited(adj.size(), 0);
+    q.push(node);
+
+    // Using BFS to print all children
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        visited[u] = 1;
+
+        for (auto &v : adj[u])
+        {
+            if (!visited[v])
+            {
+                visited[v] = 1;
+                cout << u << " -> " << v;
+                q.push(v);
+            }
+            cout << endl;
+        }
+    }
+}
+
+int main()
+{
+    int N = 7, root = 1;
+    vii adj(N + 1, vector<int>());
+
+    addEdge(1, 2, adj);
+    addEdge(1, 3, adj);
+    addEdge(1, 4, adj);
+    addEdge(2, 5, adj);
+    addEdge(2, 6, adj);
+    addEdge(4, 7, adj);
+
+    cout << "Print parents:" << endl;
+    printParents(root, 0, adj);
+    cout << "Print leaf nodes:" << endl;
+    printLeafNodes(root, adj);
+    cout << "Print children:" << endl;
+    printChildren(root, adj);
+
+    return 0;
+}
